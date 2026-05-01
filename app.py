0a18f7fc2bfc88ceb168a2a2201c2d057f2806f0
SpikeyCoder <kevinmarmstrong1990@gmail.com>
2026-05-01 09:23:59 +0000

    chore(security): drop custom Server response header
    Removes 'response.headers["Server"] = "WebAuditor"' from
    _set_security_headers(). The header advertised a non-standard
    server token without leaking version info, but disclosing any
    custom server identifier is informational fingerprinting that
    adds nothing for legitimate clients.
    
    Refs: OWASP ASVS V14.4.7, CWE-200

--- app.py
+++ app.py
@@ -149,7 +149,6 @@ def _set_security_headers(response):
         response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
         response.headers["Access-Control-Allow-Headers"] = "Content-Type, X-Requested-With"
-    response.headers["Server"] = "WebAuditor"
     return response
 
 
